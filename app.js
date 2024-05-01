0621c1491a938f018182a566cac256622c8a6113
rostyslaw2 <rostyk.shvets@gmail.com>
2024-05-01 22:00:44 +0300

    image

--- app.js
+++ app.js
@@ -3,18 +3,18 @@ const app = express();
 const ejs = require('ejs')
 const mongoose = require('mongoose')
 require("dotenv").config();
-const Cart = require("./models/Cart");
-require("dotenv").config();
+const Cart = require("./models/Cart1");
 app.engine("ejs", require("ejs").renderFile);
 app.set("view engine", "ejs");
 
 app.use(express.static(__dirname + "/views"));
-app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(express.static("public"));
 
 
-app.set('view engine', 'ejs')
-const bodyParser = require('body-parser')
+
+const bodyParser = require('body-parser');
+const Cart1 = require('./models/Cart1');
 app.use(bodyParser.urlencoded({ extended: false }))
 
 
@@ -22,12 +22,13 @@ app.get('/', function (req, res) {
   res.render('house') // render відкриває htmll файл з передачою файлів
 })
 app.post('/add', async (req, res) => { // post дістає з форми htmll
-  const name = req.body.name
-  const email = req.body.email
-  const age = req.body.age
-  const newCart = new Cart({ name, email, age })
+  //const name = req.body.name
+  //const email = req.body.email
+  //const age = req.body.age
+const { name,  age } = req.body // заміна верхніх 3 рядків
+  const newCart = new Cart({ name,  age })
   await newCart.save()
-  res.render('result', { name, email, age })
+  res.render('result', { name,  age })
 })
 
 
@@ -36,6 +37,11 @@ app.get('/basa', function (req, res) { // get просто відкриває ф
 })
 
 console.log('Example app listening on port 3000!')
+
+app.get('/all', async(req, res) => { // get просто відкриває файл
+  const AllCart = await Cart1.find() // ми дістаємо всіх користовачів з моделі Cart1
+  res.render('all', { AllCart }) // відкриваєм і передаємо
+})
 
 
 const start = async () => { // робить асинхронну функцію
